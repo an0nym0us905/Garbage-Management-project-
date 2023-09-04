c42c94fbb9e4ffc1548d7d9b02f5d21efffd1bb5
an0nym0us905 <144034702+an0nym0us905@users.noreply.github.com>
2023-09-04 19:40:41 +0530

    Update garbagemsdb (1).sql

--- garbagemsdb (1).sql
+++ garbagemsdb (1).sql
@@ -1,6 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
+
 --
 -- Host: 127.0.0.1
 -- Generation Time: Aug 14, 2022 at 08:15 PM
@@ -10,12 +8,6 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `garbagemsdb`
